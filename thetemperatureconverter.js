a29baeb7ee766da173327b6211f581c8172daa55
Ava Ryerson <ava.ryerson@gmail.com>
2016-12-13 16:27:46 -0800

    The temperature converter exercise

--- thetemperatureconverter.js
+++ thetemperatureconverter.js
@@ -7,3 +7,22 @@
 
 // Call console.log function with your output value
 // when you are ready.
+/*
+	It's hot out! Let's make a converter based on the steps here.
+
+	Store a celsius temperature into a variable.
+	Convert it to fahrenheit and output "NN°C is NN°F".
+	Now store a fahrenheit temperature into a variable.
+	Convert it to celsius and output "NN°F is NN°C."
+*/
+var celsiusTemp = 50;
+var convertFarenheit = celsiusTemp * (9/5) + 32;
+var answer = convertFarenheit + "°F is " + celsiusTemp + "°C.";
+console.log(answer);
+
+
+// Ava Ryerson
+// var firstName = "Ava";
+// var lastName = "Ryerson";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
