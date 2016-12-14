13a5536a5addb8bd5120d7ff800be306338c08bf
Ava Ryerson <ava.ryerson@gmail.com>
2016-12-13 17:49:34 -0800

    the lifetime supply calculator exercise

--- thelifetimesupplycalculator.js
+++ thelifetimesupplycalculator.js
@@ -1,5 +1,5 @@
 // YOUR CODE GOES HERE
-
+/*
 var currentAge = 32;
 var maxAge = 103;
 var amountPerDay = 3;
@@ -13,6 +13,59 @@ var lifetimeSupply = "You will need "
                    + "."
 
 console.log(lifetimeSupply);                
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+	Write a function named calculateSupply that:
+	[X] - takes 2 arguments: age, amount per day.
+	[X] - calculates the amount consumed for rest of the life (based on a constant max age).
+	[X] - outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+	[X] - Call that function three times, passing in different values each time.
+	Bonus: Accept floating point values for amount per day, and round the result to a round number.
+*/
+
+var MAX_AGE = 39;
+var DAYS_PER_YEAR = 365;
+
+function yearsToDays(years) {
+  return years * DAYS_PER_YEAR;
+}
+
+function calculateSupply(age, amountPerDay) {
+  
+  var yearsRemaining = MAX_AGE - age;
+  var daysRemaining = yearsToDays(yearsRemaining);
+  var amountReq = amountPerDay * daysRemaining;
+
+  var calculateSupply = "You will need "
+                   + amountReq
+                   + " to last you until the ripe old age of "
+                   + MAX_AGE
+                   + "."
+
+  console.log(calculateSupply)
+}
+
+
+
+calculateSupply(27, 3);
+calculateSupply(17, 6);
+calculateSupply(7, 7);
+
 
 // Call the console.log function with your output value
 // when you are ready.
