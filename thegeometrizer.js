29af25a97c5267ee75d0472e1ec2df7ed329537e
Ava Ryerson <ava.ryerson@gmail.com>
2016-12-13 17:02:47 -0800

    The Geometrizer exercise

--- thegeometrizer.js
+++ thegeometrizer.js
@@ -7,3 +7,21 @@
 
 // Call the console.log function with your output value
 // when you are ready.
+/*
+  Calculate properties of a circle, using the definitions here.
+
+Store a radius into a variable.
+Calculate the circumference based on the radius, and output "The circumference is NN".
+Calculate the area based on the radius, and output "The area is NN".
+*/
+
+var radius = 48;
+var circumferenceCircle = 2 * Math.PI * radius;
+var areaCircle = 2 * Math.PI * radius * radius;
+
+var circAnswer = "The circumference is " + circumferenceCircle + ".";
+var areaAnswer = "The area is " + areaCircle + ".";
+
+
+console.log(circAnswer);
+console.log(areaAnswer);
